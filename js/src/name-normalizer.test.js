b351e4a3ae8ffa269382d17aa57841bcec508b6a
Josh Justice <me@codingitwrong.com>
2020-12-12 06:26:34 -0500

    Prettify

--- js/src/name-normalizer.test.js
+++ js/src/name-normalizer.test.js
@@ -2,38 +2,44 @@ import { normalize } from './name-normalizer';
 
 describe('a name normalizer', () => {
   it('returns empty when passed empty string', () => {
-    expect(normalize("")).toEqual("");
+    expect(normalize('')).toEqual('');
   });
 
   xit('returns single word name', () => {
-    expect(normalize("Plato")).toEqual("Plato");
+    expect(normalize('Plato')).toEqual('Plato');
   });
 
   xit('swaps first and last names', () => {
-      expect(normalize("Haruki Murakami")).toEqual("Murakami, Haruki");
+    expect(normalize('Haruki Murakami')).toEqual('Murakami, Haruki');
   });
 
   xit('trims leading and trailing whitespace', () => {
-      expect(normalize("  Big Boi   ")).toEqual("Boi, Big");
+    expect(normalize('  Big Boi   ')).toEqual('Boi, Big');
   });
 
   xit('initializes middle name', () => {
-      expect(normalize("Henry David Thoreau")).toEqual("Thoreau, Henry D.");
+    expect(normalize('Henry David Thoreau')).toEqual('Thoreau, Henry D.');
   });
 
   xit('does not initialize one letter middle name', () => {
-      expect(normalize("Harry S Truman")).toEqual("Truman, Harry S");
+    expect(normalize('Harry S Truman')).toEqual('Truman, Harry S');
   });
 
   xit('initializes each of multiple middle names', () => {
-      expect(normalize("Julia Scarlett Elizabeth Louis-Dreyfus")).toEqual("Louis-Dreyfus, Julia S. E.");
+    expect(normalize('Julia Scarlett Elizabeth Louis-Dreyfus')).toEqual(
+      'Louis-Dreyfus, Julia S. E.',
+    );
   });
 
   xit('appends suffixes to end', () => {
-      expect(normalize("Martin Luther King, Jr.")).toEqual("King, Martin L., Jr.");
+    expect(normalize('Martin Luther King, Jr.')).toEqual(
+      'King, Martin L., Jr.',
+    );
   });
 
   xit('throws when name contains two commas', () => {
-    expect(() => { normalize("Thurston, Howell, III") }).toThrow();
+    expect(() => {
+      normalize('Thurston, Howell, III');
+    }).toThrow();
   });
 });
