ef3a959668f124ee4305e4d52f044977c1207145
Josh Justice <me@codingitwrong.com>
2020-12-15 20:17:19 -0500

    Refactoring

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -1,15 +1,10 @@
 export const normalize = name => {
-  let nameBeforeSuffix;
-  let suffix;
-  let extraneousCommaSegment;
-  [nameBeforeSuffix, suffix, extraneousCommaSegment] = name.split(', ');
-  if (extraneousCommaSegment) {
+  const commaTokens = name.split(', ');
+  if (commaTokens.length > 2) {
     throw 'name may only contain a max of one comma';
   }
 
-  if (!suffix) {
-    nameBeforeSuffix = name;
-  }
+  const [nameBeforeSuffix, suffix] = commaTokens;
 
   const names = nameBeforeSuffix.trim().split(' ');
   if (names.length <= 1) {
