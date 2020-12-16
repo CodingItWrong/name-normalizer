07aa4b3cf502b1d6785719b84f4480fe1364ff2d
Josh Justice <me@codingitwrong.com>
2020-12-15 20:10:27 -0500

    WIP

--- js/src/name-normalizer.js
+++ js/src/name-normalizer.js
@@ -1,3 +1,39 @@
-export const normalize = (/*name*/) => {
-  return 'wha?';
+export const normalize = name => {
+  let nameBeforeSuffix;
+  let suffix;
+  let extraneousCommaSegment;
+  [nameBeforeSuffix, suffix, extraneousCommaSegment] = name.split(', ');
+  if (extraneousCommaSegment) {
+    throw 'name may only contain a max of one comma';
+  }
+
+  if (!suffix) {
+    nameBeforeSuffix = name;
+  }
+
+  const names = nameBeforeSuffix.trim().split(' ');
+  if (names.length <= 1) {
+    return name;
+  }
+
+  const firstName = first(names);
+  const middleNames = names.slice(1, -1);
+  const lastName = last(names);
+  const middleInitials = middleNames.map(initialize);
+
+  const namesOtherThanLast = [firstName, ...middleInitials];
+  return `${lastName}, ${namesOtherThanLast.join(' ')}${
+    suffix ? `, ${suffix}` : ''
+  }`;
+};
+
+const first = array => array[0];
+const last = array => array[array.length - 1];
+
+const initialize = name => {
+  if (name.length > 1) {
+    return `${name.slice(0, 1)}.`;
+  } else {
+    return name;
+  }
 };
